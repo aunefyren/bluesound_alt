ff133721f5d3b47c162a42ce624b35806cdb730f
aunefyren <oystein.sverre@gmail.com>
2024-02-04 15:45:44 +0100

    Update config_flow.py

--- custom_components/bluesound_alt/config_flow.py
+++ custom_components/bluesound_alt/config_flow.py
@@ -13,7 +13,7 @@ from homeassistant.const import (
     CONF_NAME,
 )
 
-from .media_player import (DEFAULT_PORT, BluesoundPlayer, _add_player, on_updated_cb)
+from .media_player import (DEFAULT_PORT, BluesoundPlayer, on_updated_cb)
 from .const import DOMAIN  # pylint:disable=unused-import
 
 _LOGGER = logging.getLogger(__name__)
@@ -53,7 +53,7 @@ async def validate_input(hass: HomeAssistant, data: dict) -> dict[str, Any]:
     if len(data[CONF_NAME]) < 1:
         raise InvalidName
 
-    player = BluesoundPlayer(HomeAssistant, data[CONF_HOST], DEFAULT_PORT, data[CONF_NAME], _add_player_cb)
+    player = BluesoundPlayer(HomeAssistant, data[CONF_HOST], DEFAULT_PORT, data[CONF_NAME], None)
     result = await player.force_update_sync_status(on_updated_cb, raise_timeout=False)
     if result is not True:
         # If there is an error, raise an exception to notify HA that there was a
