c1ddd8383da037501617a8b33ffcea28eaaa2cf0
aunefyren <oystein.sverre@gmail.com>
2024-02-05 14:25:15 +0100

    Update __init__.py

--- custom_components/bluesound_alt/__init__.py
+++ custom_components/bluesound_alt/__init__.py
@@ -3,8 +3,13 @@ from .media_player import (async_setup_platform)
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.config_entries import ConfigEntry
+import logging
+
+_LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up a config entry for Bluesound."""
+    _LOGGER.debug("Starting entry: %s", entry)
+
     await async_setup_platform(hass, entry, AddEntitiesCallback, None)
     return True
